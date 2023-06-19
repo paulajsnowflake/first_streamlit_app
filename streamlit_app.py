ca8298470935a50331faf9085b8f3cb5b707604a
paulajsnowflake <136513933+paulajsnowflake@users.noreply.github.com>
2023-06-19 14:03:45 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,6 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.jason()) #just writes data to the screen
 
 #take the json version of the response and normalize it
 # write your own comment -what does the next line do? 
